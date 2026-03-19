0655bc909e52172a66049a40e8526c9c4793bd48
Geetansha <gudugarg193@gmail.com>
2026-03-20 01:00:52 +0530

    Corrected resume lookup key

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -62,11 +62,11 @@ const Resume = () => {
         </Link>
       </nav>
       <div className="flex flex-row w-full max-lg:flex-col-reverse">
-        <section className="feedback-section  bg-[url('/images/bg-small.svg' bg-cover h-[100vh] sticky top-0 items-center justify-center">
+        <section className="feedback-section  bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
           {imageUrl && resumeUrl && (
             <div className="fade-in duration-1000 gradient-border  max-sm:m-0 h-[90%] max-2xl:h-fit w-fit">
               <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
-                <img src={imageUrl} title="resume" className="w-full h-full object-contain rounded-full"/>
+                <img src={imageUrl} title="resume" className="w-full h-full object-contain rounded-2xl"/>
               </a>
             </div>
           )}
